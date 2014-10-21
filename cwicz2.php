61ff41968df03d77fd9966919f10c79a7e768e8b
Pan Otlet <otlet0@gmail.com>
2014-10-21 12:10:21 +0200

    Zadanie kolejne :bomb:

--- cwicz2.php
+++ cwicz2.php
@@ -13,7 +13,21 @@
   $tab[2]="Czechy";
   $tab[3]="Francja";
   printf ("Nowi członkowie Unii Europejskiej to: ".$tab[0]." i ".$tab[2].", a starzy trutnie to: ".$tab[1]." i ".$tab[3]);
-  
+  echo ("<br /><br />");
+
+  $x="Renata";
+  $y="Beger";
+  $z="$x $y";
+  echo ("<br /><br />");
+
+  $tab[0]="Darek";
+  $tab[1]="Marek";
+  $tab[2]="Ewa";
+  $tab[3]="Ola";
+  $i=4;
+  $tab[$i]="Ania";
+  printf ("<b>$z i jej czworo dzieci:</b><br />$tab[0]<br />$tab[1]<br />$tab[2]<br />$tab[3]</br>$tab[4]");
+  echo ("<br /><br />");
 
   include 'include/footer.php';
 ?>

c8cb2c2bac687a440f8c6af0c63c9e5f867d5fca
Pan Otlet <otlet0@gmail.com>
2014-10-27 10:07:01 +0100

    Wi�nieski to pizda :laughing:

--- cwicz2.php
+++ cwicz2.php
@@ -29,5 +29,38 @@
   printf ("<b>$z i jej czworo dzieci:</b><br />$tab[0]<br />$tab[1]<br />$tab[2]<br />$tab[3]</br>$tab[4]");
   echo ("<br /><br />");
 
+  $x="Janek";
+  $y="Wisniewski";
+  echo ("$x $y");
+  echo ("<br /><br />");
+
+  $a=5;
+  echo($a);
+  $a+=3;
+  echo($a);
+  $a-=1;
+  echo($a);
+  $a*=4;
+  echo($a);
+  $a/=2;
+  echo($a);
+  $a%=3;
+  echo($a);
+  echo ("<br />");
+  $a=5;
+  echo($a."<br />");
+  $a+=3;;
+  echo($a."<br />");
+  $a-=1;;
+  echo($a."<br />");
+  $a*=4;;
+  echo($a."<br />");
+  $a/=2;;
+  echo($a."<br />");
+  $a%=3;;
+  echo($a."<br />");
+
+  echo ("<br /><br />");
+
   include 'include/footer.php';
 ?>
--- include/footer.php
+++ include/footer.php
@@ -10,6 +10,5 @@
   <footer style="text-align: center">
     <h1>&copy <?php $otlet='<a href="http://otlet.pl" target="_blank">Pan Otlet</a>'; echo (sqrt((date(Y)))*sqrt((date(Y)))); echo (" $otlet"); ?></h1>
   </footer>
-  </body>
 
 </html>

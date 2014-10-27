99cdd5635e34fdd962144228d93fdfa362f2410a
Pan Otlet <otlet0@gmail.com>
2014-10-27 09:58:49 +0100

    Ma�a poprawka style:center :poop:

--- include/footer.php
+++ include/footer.php
@@ -7,8 +7,8 @@
 
       <!-- Bootstrap Core JavaScript -->
       <script src="js/bootstrap.min.js"></script>
-  <footer>
-    <h1 style="text-align: center">&copy <?php $otlet='<a href="http://otlet.pl" target="_blank">Pan Otlet</a>'; echo (sqrt((date(Y)))*sqrt((date(Y)))); echo (" $otlet"); ?></h1>
+  <footer style="text-align: center">
+    <h1>&copy <?php $otlet='<a href="http://otlet.pl" target="_blank">Pan Otlet</a>'; echo (sqrt((date(Y)))*sqrt((date(Y)))); echo (" $otlet"); ?></h1>
   </footer>
   </body>
 

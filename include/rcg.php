b915f0653650e3fd9575789e55aff0635b072b99
PudzianKing <jmbatkowski@gmail.com>
2014-11-14 10:04:39 +0100

    :collision:

---
+++ include/rcg.php
@@ -0,0 +1,15 @@
+<?php
+  function rcg()
+  {
+    $color="#";
+    for($j=0;$j<3;$j++)
+    {
+      $color .=dechex ( rand (0,256));
+    }
+    return $color;
+  }
+  $color=rcg();
+  echo "<style>
+  body {background-color:$color}
+  </style>";
+?>
